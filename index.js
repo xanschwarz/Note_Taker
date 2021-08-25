804c68571d152bb2de93ddcce18e87d13f258eda
Xander <xanschwarz@gmail.com>
2021-08-24 23:04:10 -0700

    Rewrote new note function and post and combined them within just the post request. Added the delete request in a similar fashion.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require('./db/db.json');
+let db = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 dbPath = './db/db.json';
 
@@ -21,28 +21,30 @@ app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
-// Post request for new note.
+// API post receives new note to save on request body, adds to db, returns to client. Each note gets a unique ID with uuid module.
 app.post('/api/notes', (req, res) => {
-    const genNewNote = newNote(req.body, db);
-    res.json(genNewNote);
-});
-
-// Function for new note.
-function newNote(body, notes) {
     // Set user input and existing notes to variables. Add a unique ID to the new note and push the note to the collected notes array.
-    const noteToAdd = body;
+    const noteToAdd = req.body;
     noteToAdd.id = uuidv4();
-    const notesArr = notes || [];
+    const notesArr = db || [];
     notesArr.push(noteToAdd);
 
     // Write the collected notes to the db.json and return the added note.
     fs.writeFileSync(path.join(__dirname, dbPath), JSON.stringify(notesArr));
-    return noteToAdd;
-}
+    res.json(noteToAdd);
+});
 
-// function to delete note
+// API delete receives ID of the note to be deleted, filters it out, and rewrites db without the deleted note.
+app.delete('/api/notes/:id', (req, res) => {
+    // Rewrite db to include only those notes with ID's that don't match the one to be deleted.
+    db = db.filter((note) => {
+        return note.id != req.params.id;
+    });
 
-// delete request
+    // Write the collected notes to the db.json.
+    fs.writeFileSync(path.join(__dirname, dbPath), JSON.stringify(db));
+    res.json(true);
+});
 
 // HTML get routes.
 app.get('/notes', (req, res) => {
