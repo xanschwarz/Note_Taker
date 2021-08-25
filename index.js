41f0b643a7e138bb015f0effc6511dd80df5ffb1
Xander <xanschwarz@gmail.com>
2021-08-24 17:09:23 -0700

    Port designated. Express assigned to const app.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('/db/db.json');
+
+// Uses environment port, or 3001 if not environment port not specified.
+const PORT = process.env.port || 3001;
+const app = express();
