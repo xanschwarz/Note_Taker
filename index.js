df1d5bb8423ce194d4296c0c7377ef8810c0c3c8
Xander <xanschwarz@gmail.com>
2021-08-24 22:24:07 -0700

    Function for new note added and functional.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Imports to variables. Express used throughout. fs used to write to db for data persistence. Path used for routes.
+// Imports necessary modules. Express used throughout. fs used to write to db for data persistence. Path used for routes.
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
-dbPath = './db/db.json';
 const { v4: uuidv4 } = require('uuid');
+dbPath = './db/db.json';
 
 // Uses environment port, or 3001 if not environment port not specified.
 const PORT = process.env.port || 3001;
@@ -15,6 +15,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// API get route.
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+// Post request for new note.
+app.post('/api/notes', (req, res) => {
+    const genNewNote = newNote(req.body, db);
+    res.json(genNewNote);
+});
+
+// Function for new note.
+function newNote(body, notes) {
+    // Set user input and existing notes to variables. Add a unique ID to the new note and push the note to the collected notes array.
+    const noteToAdd = body;
+    noteToAdd.id = uuidv4();
+    const notesArr = notes || [];
+    notesArr.push(noteToAdd);
+
+    // Write the collected notes to the db.json and return the added note.
+    fs.writeFileSync(path.join(__dirname, dbPath), JSON.stringify(notesArr));
+    return noteToAdd;
+}
+
+// function to delete note
+
+// delete request
 
 // HTML get routes.
 app.get('/notes', (req, res) => {
@@ -28,30 +56,6 @@ app.get('*', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-// API get route.
-// For below, is db.slice(1) necessary?
-app.get('/api/notes', (req, res) => {
-    res.json(db);
-});
-
-// post request
-app.post('/api/notes', (req, res) => {
-    const genNewNote = newNote(req.body, db);
-    res.json(genNewNote);
-});
-
-function newNote(body, notes) {
-    // code
-}
-
-// post request for new note
-
-// function to delete note
-
-// delete request
-
-
 
 // Start the server.
 app.listen(PORT, () =>
