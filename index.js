a3014ec6979da2aabcb0e7ae447eac8d8b7ebd01
Xander <xanschwarz@gmail.com>
2021-08-24 17:07:46 -0700

    Modules imported.

---
+++ index.js
@@ -0,0 +1,5 @@
+// Imports to variables. Express used throughout. fs used to write to db for data persistence. Path used for routes.
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('/db/db.json');
